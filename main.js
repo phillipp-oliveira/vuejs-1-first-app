dc62253b9db3d2e6ab8f8a98fcfc3ceceb52ae9f
Phillipp Oliveira <info.phillipp@gmail.com>
2017-10-03 00:56:36 -0300

    add methods, event listeners and use Vue directives

--- main.js
+++ main.js
@@ -1,34 +1,45 @@
 const app = new Vue({
   el: "#app",
   data: {
-    john: {
-      first: "Jhon",
-      last: "Doe",
-      age: 28,  
-    },
-    jane: {
-      first: "Jane",
-      last: "Doe",
-      age: 21,
-    }
-  },
-  computed: {
+    friends: [
+      {
+        first: "John",
+        last: "Doe",
+        age: 28,  
+      },
+      {
+        first: "Jane",
+        last: "Doe",
+        age: 21,
+      },
+    ], 
   },
   filters: {
     fullName(obj){
-      return `${obj.first } ${obj.last}`
+      return `${obj.last }, ${obj.first}`
     },
     ageInOneYear(obj){
       return obj.age + 1;
     }
   },
+  methods: {
+    incrementAge(friend){
+      friend.age = friend.age +1;
+    },
+    decrementAge(friend){
+      friend.age = friend.age -1;
+    }
+  },
   template: `
     <div>
-      <h1>first: {{john | fullName}}</h1>
-      <h2>Age: {{john | ageInOneYear}}</h2>
-      <hr>
-      <h1>first: {{jane | fullName}}</h1>
-      <h2>Age: {{jane | ageInOneYear}}</h2>
+      <h2 v-for="friend in friends"> 
+        <h4>{{friend | fullName}}</h4>
+        <h5>age: {{friend.age}}</h5>
+        <button v-on:click="decrementAge(friend)">-</button>
+        <button v-on:click="incrementAge(friend)">+</button>
+        <input v-model="friend.first" />
+        <input v-model="friend.last" />
+      </h2>
     </div>
   `
 })
